ce8ceba7f6fa4d9ba7de79f3e75b38133b7e4d1d
datpq <datphamquoc92@gmail.com>
2025-07-24 05:50:07 +0700

    - Use .env.production file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ interface HeaderProps {
   initializeCounter?: number;
 }
 const isProd = process.env.NODE_ENV === 'production';
+const basePath = isProd ? process.env.NEXT_PUBLIC_PRODUCTION_PATH : '';
 
 const Header: React.FC<HeaderProps> = ({initializeCounter = 0}) => {
   const [count, setCount] = useState(initializeCounter);
@@ -17,7 +18,7 @@ const Header: React.FC<HeaderProps> = ({initializeCounter = 0}) => {
       <h1 className='text-lime-300'>Next.js 15 Micro Frontend Header Auto update {count}</h1>
       <button onClick={handleClick}>Increment +1</button>
       <img
-        src={isProd ? `${process.env.PRODUCTION_PATH}/next.svg` : '/next.svg'}
+        src={`${basePath}/next.svg`}
         alt="Next.js Logo"
         className="dark:invert"
         width={180}
